b2660d6e5d59ee6567e06568112dae6ac3efebd4
Nguyen Tuan Cuong <cuong.nguyentuan@digia.com>
2020-06-13 17:37:53 +0300

    WIP

--- packages/react-components/.storybook/main.js
+++ packages/react-components/.storybook/main.js
@@ -1,4 +1,14 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/addon-viewport',
+    '@storybook/addon-backgrounds/register',
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-actions',
+    '@storybook/addon-storysource',
+    '@storybook/addon-links',
+    '@storybook/preset-typescript',
+  ],
 };
---
+++ packages/react-components/src/Test.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export const Abc: React.FunctionComponent = () => {
+    return <a>abc</a>
+}
